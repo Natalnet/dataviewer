a8f8bb229b50ca7175cfc1736c6682b29b9d0ebb
Rafael Franco <rafaelribeirofranco4@gmail.com>
2020-11-23 19:33:30 -0300

    Correções simples nos gráficos

--- src/components/Grafic/StudentGraficByMean.jsx
+++ src/components/Grafic/StudentGraficByMean.jsx
@@ -26,7 +26,7 @@ function App(props) {
         }}
       >
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis name={0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10} />
+        <XAxis />
         <YAxis unit="%"/>
         <Tooltip />
         <Legend />
