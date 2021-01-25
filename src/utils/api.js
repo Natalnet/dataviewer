be177b0387736d61fce426fd729eafb7b4b5eae9
Rafael Franco <rafaelribeirofranco4@gmail.com>
2021-01-25 11:43:16 -0300

    Atualizando a base url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "http://restapidataview.herokuapp.com/"
+  baseURL: "https://restapidataview.herokuapp.com/"
 });
 
 export default api;
