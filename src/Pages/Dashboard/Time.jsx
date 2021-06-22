7bfb1b05cf454a40a66077d0b07ea6fa2e3554c7
Rafael Franco <rafaelribeirofranco4@gmail.com>
2021-06-22 18:19:13 -0300

    Evitando problemas

--- src/Pages/Dashboard/Time.jsx
+++ src/Pages/Dashboard/Time.jsx
@@ -7,8 +7,6 @@ import maxDayTime from '../../json/df_max_day_time.json';
 import questionSecounds from '../../json/df_questions_secounds.json';
 import maxDayTimeSubject from '../../json/classQuestionsMaxTimeAndDaysAttemptPerSubject.json';
 import maxDayTimeDifficulty from '../../json/df_MaxConsuming.json';
-import { useLocation } from 'react-router-dom';
-import Histogram from '../../components/Lists/Histogram';
 
 
 const useStyles = makeStyles({
@@ -25,8 +23,6 @@ export default function App() {
   const [option, setOption] = useState(1);
   const firstOption = "Turma";
   const secondOption = "Histograma";
-  const location = useLocation();
-  const {graphs}=location.state;
 
   //Retorna a tela de escolha do usuário, dentre Histograma e turma
   function handleClick(option) {
@@ -45,8 +41,8 @@ export default function App() {
 
           <ViewChartTime dataQuestion={maxDayTime} dataSubmissions={questionSecounds}
             dataSubject={maxDayTimeSubject} dataDifficulty={maxDayTimeDifficulty} />
-          :
-          <Histogram dataList={graphs.GTDGL} dataSubject={graphs.GTDGA} dataDifficulty={graphs.GTDGD} />
+            : ''
+            /*<Histogram dataList={graphs.GTDGL} dataSubject={graphs.GTDGA} dataDifficulty={graphs.GTDGD} />*/
         }
       </CardTime>
     </Container>
